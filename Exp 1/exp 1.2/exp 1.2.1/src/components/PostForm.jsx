739b56fe73b243ef908e8a344590d499648ed0dd
ANIL KUMAR <ak8488644@gmail.com>
2026-07-20 14:39:30 +0530

    Create PostForm.jsx

---
+++ Exp 1/exp 1.2/exp 1.2.1/src/components/PostForm.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { addPost } from "../features/posts/postSlice";
+
+const PostForm = () => {
+
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [platform, setPlatform] = useState("Facebook");
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+
+    if (title.trim() === "") {
+
+      alert("Enter post title");
+
+      return;
+
+    }
+
+    dispatch(
+      addPost({
+        title,
+        platform,
+      })
+    );
+
+    setTitle("");
+    setPlatform("Facebook");
+
+  };
+
+  return (
+
+    <form className="post-form" onSubmit={handleSubmit}>
+
+      <input
+        type="text"
+        placeholder="Enter post title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <select
+        value={platform}
+        onChange={(e) => setPlatform(e.target.value)}
+      >
+        <option>Facebook</option>
+        <option>Instagram</option>
+        <option>Twitter</option>
+        <option>LinkedIn</option>
+      </select>
+
+      <button type="submit">
+
+        Add Post
+
+      </button>
+
+    </form>
+
+  );
+
+};
+
+export default PostForm;
