d4b9722c27cfdef97619b00188292f6fb4acc6f5
ANIL KUMAR <ak8488644@gmail.com>
2026-07-20 16:26:18 +0530

    Create Header.jsx

---
+++ Exp 1/exp 1.3/exp 1.3.1/src/components/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+
+  return (
+
+    <div>
+
+      <h3>
+
+        JWT Authentication Demo
+
+      </h3>
+
+      <hr />
+
+      <br />
+
+    </div>
+
+  );
+
+};
+
+export default Header;
