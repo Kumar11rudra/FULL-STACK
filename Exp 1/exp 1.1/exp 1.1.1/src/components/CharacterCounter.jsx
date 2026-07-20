bfe0c72c857d61bbcda78684faeb53814ff68bf4
ANIL KUMAR <ak8488644@gmail.com>
2026-07-20 13:00:21 +0530

    Create CharacterCounter.jsx

---
+++ Exp 1/exp 1.1/exp 1.1.1/src/components/CharacterCounter.jsx
@@ -0,0 +1,35 @@
+import platforms from "../data/platforms";
+
+const CharacterCounter = ({ text, selectedPlatforms }) => {
+
+  return (
+
+    <div className="character-counter">
+
+      <h3>Character Counter</h3>
+
+      <p>
+        Total Characters : <strong>{text.length}</strong>
+      </p>
+
+      {
+
+        selectedPlatforms.map((platform) => (
+
+          <p key={platform}>
+
+            {platform} : {text.length}/{platforms[platform].limit}
+
+          </p>
+
+        ))
+
+      }
+
+    </div>
+
+  );
+
+};
+
+export default CharacterCounter;
