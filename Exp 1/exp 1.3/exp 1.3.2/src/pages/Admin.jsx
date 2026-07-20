1de99c06d7c8f40a04b450abc8824129767626ff
ANIL KUMAR <ak8488644@gmail.com>
2026-07-20 17:03:54 +0530

    Create Admin.jsx

---
+++ Exp 1/exp 1.3/exp 1.3.2/src/pages/Admin.jsx
@@ -0,0 +1,22 @@
+const Admin = () => {
+
+  return (
+
+    <div className="page">
+
+      <h2>Admin Dashboard</h2>
+
+      <p>
+
+        Admin can manage users,
+        roles and application settings.
+
+      </p>
+
+    </div>
+
+  );
+
+};
+
+export default Admin;
