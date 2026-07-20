cef2a93dd70be4df13106bb242ba94913719dff0
ANIL KUMAR <ak8488644@gmail.com>
2026-07-20 17:04:20 +0530

    Create Editor.jsx

---
+++ Exp 1/exp 1.3/exp 1.3.2/src/pages/Editor.jsx
@@ -0,0 +1,22 @@
+const Editor = () => {
+
+  return (
+
+    <div className="page">
+
+      <h2>Editor Dashboard</h2>
+
+      <p>
+
+        Editor can create,
+        update and publish content.
+
+      </p>
+
+    </div>
+
+  );
+
+};
+
+export default Editor;
