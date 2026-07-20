fb837d5a29be41bea6b67d59bcd5a5f8da15fa72
ANIL KUMAR <ak8488644@gmail.com>
2026-07-20 17:03:30 +0530

    Create Home.jsx

---
+++ Exp 1/exp 1.3/exp 1.3.2/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+const Home = () => {
+
+  return (
+
+    <div className="page">
+
+      <h1>
+
+        Role-Based Access Control
+
+      </h1>
+
+      <p>
+
+        This application demonstrates
+        protected routes using
+        Admin, Editor and Viewer roles.
+
+      </p>
+
+    </div>
+
+  );
+
+};
+
+export default Home;
