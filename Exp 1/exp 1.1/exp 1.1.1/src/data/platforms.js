974ba2168cddb85272e95225c01b8867847642fa
ANIL KUMAR <ak8488644@gmail.com>
2026-07-20 13:01:18 +0530

    Add platforms.js with social media limits and media support

---
+++ Exp 1/exp 1.1/exp 1.1.1/src/data/platforms.js
@@ -0,0 +1,37 @@
+const platforms = {
+
+  Twitter: {
+
+    limit: 280,
+
+    media: true
+
+  },
+
+  Facebook: {
+
+    limit: 5000,
+
+    media: true
+
+  },
+
+  Instagram: {
+
+    limit: 2200,
+
+    media: true
+
+  },
+
+  LinkedIn: {
+
+    limit: 3000,
+
+    media: true
+
+  }
+
+};
+
+export default platforms;
