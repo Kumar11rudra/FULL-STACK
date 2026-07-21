a61a538c7c70e8cbd73f2866d3778dff7c075d56
ANIL KUMAR <ak8488644@gmail.com>
2026-07-21 14:09:27 +0530

    Create sampleEvents.js

---
+++ Exp 1/exp 1.4/exp 1.4.1/src/data/sampleEvents.js
@@ -0,0 +1,34 @@
+const sampleEvents = [
+  {
+    id: "1",
+    title: "Instagram Campaign",
+    start: "2026-07-22",
+    color: "#2563eb"
+  },
+  {
+    id: "2",
+    title: "Facebook Promotion",
+    start: "2026-07-24",
+    color: "#16a34a"
+  },
+  {
+    id: "3",
+    title: "LinkedIn Article",
+    start: "2026-07-26",
+    color: "#9333ea"
+  },
+  {
+    id: "4",
+    title: "YouTube Shorts",
+    start: "2026-07-28",
+    color: "#dc2626"
+  },
+  {
+    id: "5",
+    title: "Twitter Update",
+    start: "2026-07-30",
+    color: "#f59e0b"
+  }
+];
+
+export default sampleEvents;
